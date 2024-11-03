738e78edf07dba766e7292ba93b5ac5282e4e8c8
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-11-03 22:40:27 +0000

    updated preamble

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -1,14 +1,13 @@
 #### Preamble ####
-# Purpose: Tests the structure and validity of the simulated Australian 
-  #electoral divisions dataset.
-# Author: Rohan Alexander
-# Date: 26 September 2024
+# Purpose: Tests the structure and validity of the simulated US 
+  # electoral polls dataset.
+# Author: Andrew Goh, Yisu Hou
+# Date: 3 November 2024
 # Contact: rohan.alexander@utoronto.ca
 # License: MIT
 # Pre-requisites: 
-  # - The `tidyverse` package must be installed and loaded
+  # - Packages `tidyverse`, `lubridate`, and `testthat` must be installed
   # - 00-simulate_data.R must have been run
-# Any other information needed? Make sure you are in the `starter_folder` rproj
 
 
 #### Workspace setup ####
